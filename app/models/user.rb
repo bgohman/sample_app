c71807960aef0777537dc6bf7d372778f028c165
Bradley Gohman <bradley.s.gohman@gmail.com>
2015-09-26 08:02:00 -0500

    password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
@@ -33,11 +33,38 @@ class User < ActiveRecord::Base
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
+  # Activates an account.
+    def activate
+      update_attribute(:activated,    true)
+      update_attribute(:activated_at, Time.zone.now)
+    end
+
+    # Sends activation email.
+    def send_activation_email
+      UserMailer.account_activation(self).deliver_now
+    end
+
+    # Sets the password reset attributes.
+    def create_reset_digest
+      self.reset_token = User.new_token
+      update_attribute(:reset_digest,  User.digest(reset_token))
+      update_attribute(:reset_sent_at, Time.zone.now)
+    end
+
+    # Sends password reset email.
+    def send_password_reset_email
+      UserMailer.password_reset(self).deliver_now
+    end
 
 
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
@@ -50,15 +77,6 @@ class User < ActiveRecord::Base
       self.activation_digest = User.digest(activation_token)
     end
 
-    # Activates an account.
-      def activate
-        update_attribute(:activated,    true)
-        update_attribute(:activated_at, Time.zone.now)
-      end
 
-    # Sends activation email.
-    def send_activation_email
-      UserMailer.account_activation(self).deliver_now
-    end
 
 end
