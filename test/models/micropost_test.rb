79bc65edeb8c0ed152e5ee161be04e9b5c8b48bf
Bradley Gohman <bradley.s.gohman@gmail.com>
2015-09-26 16:44:12 -0500

    images

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class MicropostTest < ActiveSupport::TestCase
 
   def setup
-    @user = users(:brad)]
+    @user = users(:brad)
     @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
